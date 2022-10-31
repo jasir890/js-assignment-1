d1d57680d32f82980713007e0ec9c0b4945f6358
jasir890 <jasirazam.800@gmail.com>
2022-10-31 13:11:21 +0500

    js assignment1

---
+++ app.js
@@ -0,0 +1,68 @@
+// alert("Hello world")
+// window.alert("Hello world")
+
+
+
+// var a="islamabad";
+// alert(a);
+
+// var a;
+
+// var a=10;
+// var b=10;
+// var c=a+b;
+// alert(c);
+
+// var a;
+// var a=10;
+// var b="jasir azam";
+// var c=b+a;
+// alert(c);
+
+// var a;
+// var a=10;
+// 	var b=20;
+// 	var c=a-b;
+// 	alert(c);
+
+// var a;
+// var a=10;
+// 	var b=20;
+// 	var c=a*b;
+// 	alert(c);
+
+// var a;
+// var a=10;
+// var b=20;
+
+// a++;
+// b--;
+
+// console.log(a);
+// console.log(b);
+
+// var a=10;
+// var b=a++;
+// console.log(b);
+// console.log(a);
+
+
+
+
+// var a=10;
+// var b=++a;
+// console.log(b);
+// console.log(a);
+
+// var a;
+// var a=10;
+// 	var b=20;
+// 	var c=(a+b)*a;
+// 	alert(c);
+
+//
+var a=10;
+var b=20;
+var c= a++ - --b + a - b + b - --a - ++a + --b;
+
+console.log(c);
